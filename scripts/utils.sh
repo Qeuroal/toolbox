3e96691a00cdf710872a2ed20ab95392daf11da1
Qeuroal <Qeuroal@163.com>
2023-08-12 15:53:37 +0800

    utils.sh

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -261,6 +261,31 @@ function backupFile() {
 }
 
 
+function getJsonValue() {
+  local json=$1
+  local key=$2
+
+  if [[ -z "$3" ]]; then
+    local num=1
+  else
+    local num=$3
+  fi
+
+  local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
+
+  echo ${value}
+}
+
+function getJsonFileValue() {
+    local fileName=$1
+    shift 1
+
+    echo $@
+
+    local data=$(cat ${fileName})
+    echo "$data"
+    getJsonValue "${data}" $@
+}
 
 
 
